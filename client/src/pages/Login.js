a44deda780ac1946743dbe0b3a9f1b78b1b37184
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-10-03 11:14:48 -0500

    add login html

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,56 @@
+import {useState} from 'react';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-    return(
-        <div>
-            This is the Login page
-        </div>
+
+    const [formState, setFormState] = useState({ username: '', password: ''})
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+
+        console.log(formState.username)
+    };
+
+    return (
+        <section>
+            <h1>Sign in</h1>
+
+            <form>
+
+                <div>
+                    <label for="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        placeholder="Username"
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div>
+                    <label for="password-login">Password:</label>
+                    <input
+                        type="password"
+                        id="password-login"
+                        placeholder="Password"
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+
+                <br />
+                <div>
+                    <button type="click">Forgot My Password</button>
+                </div>
+            </form>
+        </section>
     )
 }
 
