e1fa02a00383ca94998ca2fd6e1a4c9ff10bd226
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-10-02 20:21:30 -0500

    add Link to pages in the nav component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,8 +1,40 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
-    return(
-        <div>
-            This is the Nav
-        </div>
+    return (
+        <ul>
+            <li>
+                <Link to='/'>
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link to='/login'>
+                    Login
+                </Link>
+            </li>
+            <li>
+                <Link to='/games'>
+                    Games
+                </Link>
+            </li>
+            <li>
+                <Link to='/contact'>
+                    Contact
+                </Link>
+            </li>
+            <li>
+                {/* we will set this to logged in user eventually */}
+                <Link to='/profile/:username'>
+                    Profile
+                </Link>
+            </li>
+            <li>
+                <Link to='/donate'>
+                    Donations
+                </Link>
+            </li>
+        </ul>
     )
 }
 
