54abfd596408a2adc56b55a88b5751af1f704079
JSheleg <jessica.sheleg@gmail.com>
2021-10-04 17:30:36 -0500

    server.js setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/board-reactions
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    // useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
