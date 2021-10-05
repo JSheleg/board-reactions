bf5457fabff58f0ef2d30a7986f0dfc2712e8c65
JSheleg <jessica.sheleg@gmail.com>
2021-10-05 08:20:33 -0500

    Game TypeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,22 @@ const { gql } = require('apollo-server-express');
 
 //create our typeDefs
 const typeDefs = gql `
+    type Game {
+        _id: ID
+        game_name: String
+        category: String
+        min_num_players: Number
+        max_num_players: Number
+        avg_min_time: Number
+        avg_max_time: Number
+        game_description: String
+        favorites: [User]
+        favoriteTotal: Int
+
+    }
+
     type Query{
-        helloWorld:String
+        games: [Game]
     }
 
 `;
