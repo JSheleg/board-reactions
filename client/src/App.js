62175dda7022fe94157f78812c61279dd0f69dc4
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-10-09 17:55:20 -0500

    add apollo provider, context, & httplink to graphql to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import AllGames from './pages/AllGames';
 import Contact from './pages/Contact';
@@ -14,28 +21,47 @@ import Footer from './components/Footer';
 import SubmitGame from './pages/SubmitGame';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/games" component={AllGames} />
-          <Route exact path="/games/:name" component={SingleGame} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/profile/:username" component={Profile} />
-          <Route exact path="/submitgame" component={SubmitGame} />
-          <Route exact path="/donate" component={Donations} />
-          <Route exact path="/forgotpassword" component={ForgotPW} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-    
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/games" component={AllGames} />
+            <Route exact path="/games/:name" component={SingleGame} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/profile/:username" component={Profile} />
+            <Route exact path="/submitgame" component={SubmitGame} />
+            <Route exact path="/donate" component={Donations} />
+            <Route exact path="/forgotpassword" component={ForgotPW} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
