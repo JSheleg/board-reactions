594ce5c3410eb6b0c12502b218c99dff080c7973
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-10-09 18:02:14 -0500

    add handleFormSubmit func to allow use to login on login page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
+
 import Auth from '../utils/auth';
 import { ADD_USER } from "../utils/mutations";
 
