dc7baff33a5a94ef1528093ec850f6fc4be5e267
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-09 11:30:20 -0500

    added packages to server folder

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const expiration = '2w';
 
 module.exports = {
   authMiddleware: function({ req }) {
