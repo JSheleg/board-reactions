074f133725d40bd5c6da42e645f5cfec6b27fe8f
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-10-10 01:06:37 -0500

    add security questions form for user to provide answers

--- client/src/components/SecurityQuestions/index.js
+++ client/src/components/SecurityQuestions/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 
 import { UPDATE_PASSWORD } from '../../utils/mutations';
@@ -9,14 +10,71 @@ const SecurityQuestions = (props) => {
         variables: { username: props.username }
     })
 
-    if(!loading) {
-        console.log(data)
+    const [formState, setFormState] = useState({
+        answerOne: '',
+        answerTwo: ''
+    })
+
+    const user = data?.user || {};
+
+    console.log(user)
+
+    if (loading) {
+        return (
+            <div>
+                Loading....
+            </div>
+        )
     }
 
-    return(
-        <div>
-            Hello
-        </div>
+    if (!user.username) {
+        return (
+            <div>
+                Username not found in system.
+            </div>
+        )
+    }
+
+    // collect user inputs
+    const handleChange = event => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    }
+
+    const handleFormSubmit = event => {
+        
+    }
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <div>
+                <label htmlFor="answerOne">{user.questionOne}</label>
+                <input
+                    name='answerOne'
+                    type="answerOne"
+                    id="answerOne"
+                    placeholder="Answer"
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="answerTwo">{user.questionTwo}</label>
+                <input
+                    name='answerTwo'
+                    type="answerTwo"
+                    id="answerTwo"
+                    placeholder="Answer"
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
     )
 }
 
