2d5566bbb7c6f778313efaf0695bf558b038c5c9
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-10 22:46:22 -0500

    added payment form styling

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -2,6 +2,26 @@ import React, { useState} from 'react'
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import axios from "axios"
 
+const CARD_OPTIONS = {
+    iconStyle: "solid",
+    style: {
+            base: {
+                iconColor: "#c4f0ff",
+                color: "#fff",
+                fontWeight: 500,
+                fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+                fontSize: "16px",
+                fontSmoothing: "antialiased",
+                ":-webkit-autofill": { color: "#fce883" },
+                "::placeholder": { color: "#87bbfd" }
+            },
+            invalid: {
+                iconColor: "#ffc7ee",
+                color: "#ffc7ee"
+            }
+        }
+    }
+
 export default function PaymentForm() {
     const {success, setSuccess } = useState(false)
     const stripe = useStripe()
