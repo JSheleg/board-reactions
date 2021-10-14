5e9d1a9bea9c72b963d29a06e2525221c9e4e94b
Joshua Sands <josh.sands@mail.com>
2021-10-14 18:28:36 -0500

    added profile pictures

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,6 +6,11 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 
+import Mark from '../../assets/mark.jpg'
+import Jessica from '../../assets/jessica.jpg'
+import Jane from '../../assets/jane.jpg'
+import Josh from '../../assets/josh.jpg'
+
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
@@ -15,47 +20,53 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const itemData = [
     {
-      img: 'https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg',
+      img: Mark,
       title: 'Mark',
+      link: 'https://github.com/mjgiannelli'
     },
     {
-      img: 'https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg',
+      img: Jessica,
       title: 'Jessica',
+      link: 'https://github.com/JSheleg',
     },
     {
-      img: 'https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg',
+      img: Jane,
       title: 'Jane',
+      link: 'https://github.com/janekv20',
     },
     {
-      img: 'https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg',
+      img: Josh,
       title: 'Josh',
+      link: 'https://github.com/Joshsands',
     },
   ];
 
 const Footer = () => {
     return (
-<div>
 
-<Box sx={{ flexGrow: 1}}>
-        <Grid container spacing={0} direction="row" justifyContent="space-around" alignItems="flex-end" pt="40vw">
+<Box sx={{ flexGrow: 2}}>
+
+        <Grid container spacing={0} direction="row" justifyContent="center" alignItems="flex-end" pt="10vw">
         {/* <Grid item xs key='leftSpacer'></Grid> */}
-
+        <Grid item xs={12}>
+        <h3>CONTACT THE DEV TEAM!</h3>
+        </Grid>
       {itemData.map((item) => (
-        <Grid item xs={1} key={item.img}>
-          <Link href="www.google.com"><img
-            src={`${item.img}`}
-            srcSet={`${item.img}`}
+        <Grid item xs={6} sm={1} md={1} key={item.img} sx={{ ml: 2, mr: 2 }}>
+          <Link href={`${item.link}`}>
+            <img
+            src={item.img}
+            srcSet={item.img}
             alt={item.title}
             loading="lazy"
-            width="60px"
-            height="80px"
+            width="100vw"
+            height="100vw"
           /></Link>
         </Grid>
       ))}
     {/* <Grid item xs key='rightSpacer'></Grid> */}
     </Grid>
       </Box>
-</div>
     )
         }
 export default Footer;
